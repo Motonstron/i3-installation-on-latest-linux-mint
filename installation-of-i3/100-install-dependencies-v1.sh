da634bf3ba01c65061cebd651c4e6cecf9f2a494
Motonstron <194175+Motonstron@users.noreply.github.com>
2019-05-12 13:14:14 +0100

    Added polybar setup and configuration.

--- installation-of-i3/100-install-dependencies-v1.sh
+++ installation-of-i3/100-install-dependencies-v1.sh
@@ -24,7 +24,7 @@ sudo apt-get install -y doxygen
 #script 120
 sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev 
 sudo apt-get install -y libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
-
+sudo apt install git cmake cmake-data pkg-config libcairo2-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev
 
 echo
 echo
--- installation-of-i3/600-install-personal-settings-v1.sh
+++ installation-of-i3/600-install-personal-settings-v1.sh
@@ -10,6 +10,11 @@ set -e
 cp personal/.gtkrc-2.0 ~
 cp -r personal/.config/ ~
 
+[ -d $HOME"/.config/polybar" ] || mkdir $HOME"/.config/polybar"
+
+cp -r personal/polybar-launch.sh $HOME/.config/polybar/launch.sh
+chmod +x $HOME/.config/polybar/launch.sh
+
 echo "#########################################################"
 echo "Settings have been copied"
 echo "#########################################################"
